0e98a14de7bd7c3f21ac34fc00a4c557efe330eb
matheus23 <matheus23@users.noreply.github.com>
2025-07-28 12:49:18 +0000

    deploy: fa586fc61366193d17094b2539f1d3a446a6b4ef

---
+++ pr/5/docs/trait.impl/core/future/future/trait.Future.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["n0_future",[["impl&lt;T: <a class=\"trait\" href=\"n0_future/trait.Future.html\" title=\"trait n0_future::Future\">Future</a>&gt; <a class=\"trait\" href=\"n0_future/trait.Future.html\" title=\"trait n0_future::Future\">Future</a> for <a class=\"enum\" href=\"n0_future/enum.MaybeFuture.html\" title=\"enum n0_future::MaybeFuture\">MaybeFuture</a>&lt;T&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[362]}
